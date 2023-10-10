80c767a87368ae19df2263bc08be7bc909dce9c8
miguel <miguel_vegas.tekton@skyairline.com>
2023-10-10 17:08:14 -0500

    updated conection

--- src/infraestructure/mongo.ts
+++ src/infraestructure/mongo.ts
@@ -1,11 +1,14 @@
 import { model, connect } from "mongoose";
 import { cardSchema } from "./card.schema";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export const CardModel = model("Card", cardSchema);
 
 const run = async () => {
     await connect(
-        "mongodb+srv://db_user_miguel:sucas123@cluster0.oegom.mongodb.net/culqi?retryWrites=true&w=majority"
+        process.env.MONGO_URI ||
+            "mongodb+srv://db_user_miguel:sucas123@cluster0.oegom.mongodb.net/?retryWrites=true&w=majority"
     );
 };
 
